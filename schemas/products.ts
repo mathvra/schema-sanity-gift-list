88286312ff0a21898f12a44ac13c3c8a1dcacaf0
mathvra <matheusvram@gmail.com>
2024-01-24 23:34:31 -0300

    feat: set required fields and call help funtions

--- schemas/products.ts
+++ schemas/products.ts
@@ -1,40 +1,45 @@
-export default {
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
   name: 'products',
   type: 'document',
   title: 'Produtos',
   fields: [
-    {
+    defineField({
       name: 'name',
       type: 'string',
       title: 'Nome do Produto',
-    },
-    {
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'description',
       type: 'string',
       title: 'Descrição do Produto',
-    },
-    {
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'url',
       type: 'url',
       title: 'Link do produto',
-    },
-    {
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'isAssigned',
       type: 'boolean',
       title: 'Produto assinado',
-    },
-    {
+    }),
+    defineField({
       name: 'assignedName',
       type: 'string',
       title: 'Nome de quem assinou',
-    },
-    {
+    }),
+    defineField({
       name: 'assignedPhone',
       type: 'string',
       title: 'Telefone de quem assinou',
-    },
+    }),
   ],
   initialValue: {
     isAssigned: false,
   },
-}
+})
